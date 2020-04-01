f958c74a15cd099634c0d6557876e8033c32c043
Mason Jarr <rrajnosam@gmail.com>
2020-04-01 16:06:46 -0700

    add articles controller etc

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_03_31_215512) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
+    t.text "description"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
